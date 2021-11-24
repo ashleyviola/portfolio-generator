e8cb160f1c312d8ab221c3d0ad61895850ee2c20
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-11-24 14:46:09 -0500

    added inquirer functionality

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template');
+inquirer   
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+    
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
 
 
-fs.writeFile('index.html',generatePage(name,github), err => {
-    if (err) throw new Error(err);
 
-    console.log('Portfolio Complete! Check out index.html to see the output!');
-});
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html',generatePage(name,github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio Complete! Check out index.html to see the output!');
+// });
