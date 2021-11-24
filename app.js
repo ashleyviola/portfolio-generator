beff49606788acfd5d717b7e8eb865be066a8fd9
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-11-24 15:25:38 -0500

    added when statement for extra information

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ const promptUser = () => {
 
     return inquirer.prompt([
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide information about yourself:',
+            when: ({confirmAbout}) => {
+                if(confirmAbout){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
             type: 'input',
             name: 'name',
             message: 'What is your name?',
@@ -21,7 +39,15 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: gitHubInput => {
+                if(gitHubInput){
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub Username!")
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -47,13 +73,28 @@ const promptProject = portfoilioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project'
-            
+            message: 'What is the name of your project',
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                } else {
+                    console.log("Please enter your project name!")
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if(descriptionInput){
+                    return true;
+                } else {
+                    console.log("Please enter a description for your project!")
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -64,7 +105,15 @@ const promptProject = portfoilioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: githubLinkInput => {
+                if(githubLinkInput){
+                    return true;
+                } else {
+                    console.log("Please enter your name!")
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
